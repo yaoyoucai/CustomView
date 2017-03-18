bac7fd9ff7a8689b7fa3307925c5a80f3cf0b029
yaoyoucai <2511022042@qq.com>
2017-03-18 11:19:41 +0800

    首次提交34343

--- flowlayout/src/main/java/shbd/flowlayout/activity/MainActivity.java
+++ flowlayout/src/main/java/shbd/flowlayout/activity/MainActivity.java
@@ -3,15 +3,22 @@ package shbd.flowlayout.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 import shbd.flowlayout.view.FlowLayout;
 
 public class MainActivity extends AppCompatActivity {
     private FlowLayout mFLowLayout;
 
+    private List<String> mBooks = Arrays.asList("沙滩搁浅我们的旧时光", "女人天生高贵", "海是彩色的灰尘", "迷乱", "十年之前 十年之后", "向来缘浅 奈何情深",
+            "生不对 死不起", "属于我们的那片天空", "幸福还在么", "支离破碎", "暮春时绽放");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFLowLayout = (FlowLayout) findViewById(R.id.flow_layout)
+        mFLowLayout = (FlowLayout) findViewById(R.id.flow_layout);
+        mFLowLayout.setDatas(mBooks);
     }
 }
--- flowlayout/src/main/java/shbd/flowlayout/view/FlowLayout.java
+++ flowlayout/src/main/java/shbd/flowlayout/view/FlowLayout.java
@@ -1,8 +1,10 @@
 package shbd.flowlayout.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
@@ -19,6 +21,7 @@ import java.util.List;
  * 修改备注：
  */
 public class FlowLayout extends ViewGroup {
+    private Context mContext;
     private List<String> mDatas = new ArrayList<>();
     private List<Line> mLines = new ArrayList<>();
 
@@ -27,13 +30,21 @@ public class FlowLayout extends ViewGroup {
     //块的字体颜色
     private int mPieceTextColor = Color.BLACK;
 
+    //控件最小宽度
+    private int mMinWidth = 200;
+
     public FlowLayout(Context context) {
         this(context, null);
     }
 
     public FlowLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        addChildView(context);
+        mContext = context;
+    }
+
+    public void setDatas(List<String> datas) {
+        this.mDatas = datas;
+        addChildView(mContext);
     }
 
     /**
@@ -49,36 +60,113 @@ public class FlowLayout extends ViewGroup {
         Button button = new Button(context);
         button.setText(data);
         button.setTextColor(mPieceTextColor);
-        button.setBackgroundColor(mPieceBgColor);
-
         LayoutParams lp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         addView(button, lp);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
 
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width_mode = MeasureSpec.getMode(widthMeasureSpec);
+        int height_mode = MeasureSpec.getMode(heightMeasureSpec);
+        int width_size = MeasureSpec.getSize(widthMeasureSpec);
+        int height_size = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (width_mode == MeasureSpec.AT_MOST && width_size > mMinWidth) {
+            width_size = mMinWidth;
+        }
+
+        int childCount = getChildCount();
+        Line line = new Line();
+        mLines.clear();
+        mLines.add(line);
+        for (int index = 0; index < childCount; index++) {
+            View childView = getChildAt(index);
+            int measuredWidth = childView.getMeasuredWidth();
+            if (line.getWidth() + measuredWidth > width_size) {
+                line = new Line();
+                line.addView(childView);
+                mLines.add(line);
+            } else {
+                line.addView(childView);
+            }
+        }
+
+        //得到整个控件的高度
+        if (height_mode == MeasureSpec.AT_MOST) {
+            height_size = 0;
+            for (Line curLine : mLines) {
+                height_size += curLine.getHeight();
+            }
+        }
+        super.onMeasure(MeasureSpec.makeMeasureSpec(width_size, width_mode),
+                MeasureSpec.makeMeasureSpec(height_size, height_mode));
     }
 
     @Override
-    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        int curHeight = getPaddingTop();
+        for (int index = 0; index < mLines.size(); index++) {
+            Line line = mLines.get(index);
+            List<View> childViewList = line.getViewList();
+
+            //每两个控件之间的间距
+            int space = 0;
+           /* if (childViewList.size() != 1) {
+                space = (getMeasuredHeight() - getPaddingLeft() - getPaddingRight() - line.getWidth()) / (childViewList.size() - 1);
+            }*/
+            int curWidth = getPaddingLeft();
+            for (int i = 0; i < childViewList.size(); i++) {
+
+                View childView = childViewList.get(i);
+                childView.layout(curWidth + space * i, curHeight,
+                        curWidth + childView.getMeasuredWidth() + space * i,
+                        curHeight + line.getHeight());
+
+                curWidth += childView.getMeasuredWidth();
+
+            }
+
+            curHeight += line.getHeight();
+        }
     }
 
-    public void setDatas(List<String> datas) {
-        this.mDatas = datas;
-    }
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        canvas.drawColor(Color.RED);
+        super.dispatchDraw(canvas);}
 
     /**
      * 代表每一行
      */
-    private static class Line {
+   private static class Line {
+        private List<View> mViewList;
+
+        //行宽和高
+        private int lineWidth;
+        private int lineHeight;
+
+        public Line() {
+            mViewList = new ArrayList<>();
+        }
+
+        public List<View> getViewList() {
+            return mViewList;
+        }
+
+        public void addView(View view) {
+            mViewList.add(view);
+            lineWidth += view.getMeasuredWidth();
+            lineHeight = Math.max(lineHeight, view.getMeasuredHeight());
+        }
+
         public int getHeight() {
-            return 0;
+            return lineHeight;
         }
 
         public int getWidth() {
-            return 0;
+            return lineWidth;
         }
     }
 }
